d23cc890db91b4f9096ac0f22e07d93c0313612d
LinXia <linyianxia@hotmail.com>
2020-02-08 19:43:27 -0500

    geolocation & marker

--- RFProj/App.js
+++ RFProj/App.js
@@ -4,25 +4,73 @@ import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import {Header, Left, Button, Icon, Body, Right, Title, Fab} from 'native-base'
 import * as Font from 'expo-font'
 
+const {width, height} = Dimensions.get('window')
+const SCREEN_HEIGHT = height
+const SCREEN_WIDTH = width
+const ASPECT_RATIO = width / height
+const LATITUDE_DELTA = 0.0922
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
 
 export default class App extends React.Component {
-  state = {
-    fontLoaded: false,
-    location: {
-      latitude: 	40.730610, 
-      longitude: -73.935242,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421
+  constructor(props) {
+    super(props)
+    this.state = {
+      fontLoaded: false,
+      initialPosition: {
+        latitude: 0, 
+        longitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0
+      },
+      markerPosition: {
+        latitude: 0,
+        longitude: 0
+      }
     }
   }
-  async componentDidMount(){
-    await Font.loadAsync({
+
+  watchID: ?number = null
+ 
+   componentDidMount(){
+     Font.loadAsync({
       'Roboto_medium': require('./assets/fonts/Roboto-Medium.ttf')
     })
     this.setState({
       fontLoaded: true
     })
+
+    navigator.geolocation.getCurrentPosition((position)=>{
+      let lat = parseFloat(position.coords.latitude)
+      let long = parseFloat(position.coords.longitude)
+      let initialRegion = {
+        latitude: lat,
+        longitude: long,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }
+      this.setState({initialPosition: initialRegion})
+      this.setState({markerPosition: initialRegion})
+    }, (error) => alert(JSON.stringify(error)),
+    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
+
+    this.watchID = navigator.geolocation.watchPosition((position)=> {
+      let lat = parseFloat(position.coords.latitude)
+      let long = parseFloat(position.coords.longitude)
+      let lastRegion = {
+        latitude: lat,
+        longitude: long,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }
+      this.setState({initialPosition: lastRegion})
+      this.setState({markerPosition: lastRegion})
+    })
   }
+
+  componentWillUnmount(){
+    navigator.geolocation.clearWatch(this.watchID)
+  }
+
 recordEvent = (coordinate) =>{
   this.setState({
     location: coordinate
@@ -37,7 +85,7 @@ recordEvent = (coordinate) =>{
 
           </Left>
           <Body>
-            <Title>Find that restroom!</Title>
+            {/* <Title>Find that restroom!!</Title> */}
           </Body>
           <Right>
             <Button transparent>
@@ -48,11 +96,19 @@ recordEvent = (coordinate) =>{
         </Header> 
         <MapView
          style={{flex: 1}} 
-         initialRegion = {this.state.location}
+         region = {this.state.initialPosition}
          provider={MapView.PROVIDER_GOOGLE}
          customMapStyle={mapStyleColor}
-         onRegionChangeComplete={(coordinate)=>this.recordEvent(coordinate)}
+        //  onRegionChangeComplete={(coordinate)=>this.recordEvent(coordinate)}
           >
+            <MapView.Marker
+              coordinate={this.state.initialPosition}
+            >
+              <View style={styles.radius}>
+                <View style={styles.marker}/>
+              </View>
+            </MapView.Marker>
+
           </MapView>
           <Fab direction="left" position="bottomRight"
             style={{backgroundColor: 'green'}}>
@@ -68,6 +124,25 @@ recordEvent = (coordinate) =>{
 }
 
 const styles = StyleSheet.create({
+  radius: {
+    height: 50,
+    width: 50,
+    borderRadius: 50/2,
+    overflow: 'hidden',
+    backgroundColor: 'rgba(150,245,240,0.25)',
+    borderWidth: 1.5,
+    borderColor: 'rgba(255,85,51,0.4)',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  marker: {
+    height: 20,
+    width: 20,
+    borderWidth: 3,
+    borderRadius: 20/2,
+    borderColor: 'white',
+    backgroundColor: '#007AFF'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
